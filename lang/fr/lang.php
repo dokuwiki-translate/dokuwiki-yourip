1c9c63ad88232854e82180bdc259231ab0e1680e
Schplurtz le Déboulonné <schplurtz@laposte.net>
2024-01-03 23:26:25 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['you_use_v4']            = 'Vous utilisez une adresse IPv4 démodée';
+$lang['you_use_v6']            = 'Vous utilisez IPv6';
+$lang['ipv4_from']             = 'Connexion IPv4 depuis %s';
+$lang['ipv6_from']             = 'Connexion IPv6 depuis %s';
